e251031d7fcecbab88fff458d22d1fbf6cfda130
Xuwang Yin <xuwangyin@gmail.com>
2018-04-02 21:54:42 -0400

    add files

---
+++ Shape2D.py
@@ -0,0 +1,148 @@
+from __future__ import print_function
+
+import datetime
+import torch.utils.data
+from PIL import Image
+import os
+import os.path
+import errno
+import torch
+import json
+import codecs
+import numpy as np
+from pprint import pprint
+from pymongo import MongoClient
+from os import sys, path
+
+
+# %%
+def render_canvas(objects):
+    grid_size = 100
+    layout = []
+    for obj in objects:
+        top = obj['row'] * grid_size + 10
+        left = obj['col'] * grid_size + 10
+        width = grid_size - 20
+        height = grid_size - 20
+        label = obj['color']
+        shape = obj['shape']
+        if shape == 'square':
+            shape = 'rectangle'
+        layout.append({"left": left, "top": top, "width": width, "height": height, "label": label, "shape": shape})
+    return '#CANVAS-' + str(layout).replace("'", '"').replace(' ', '')
+
+# %%
+class Shape2D(torch.utils.data.Dataset):
+    def __init__(self, datafile):
+        self.data = json.load(open(datafile, 'r'))
+        max_seq_length = 0
+        vocab = set()
+        for d in self.data:
+            words = d['current_instruction'].split()
+            vocab = vocab.union(set(words))
+            max_seq_length = max(max_seq_length, len(words))
+        self.vocab = list(vocab)
+        self.vocab_size = len(self.vocab)
+        self.max_seq_length = max_seq_length
+        self.word_to_ix = {self.vocab[i]: i+1 for i in range(len(self.vocab))}
+        self.color2num = {'red': 0, 'green': 1, 'blue': 2}
+        self.shape2num = {'square': 0, 'circle': 1, 'triangle': 2}
+
+    def __getitem__(self, index):
+        raw_data = self.data[index]
+        prev_canvas_data = self.encode_canvas(raw_data['prev_canvas'])
+        inst_data = self.encode_inst(raw_data['current_instruction'])
+        next_obj_data = self.encode_obj(raw_data['next_object']).astype(np.int64)
+        final_canvas_data = self.encode_canvas(raw_data['final_canvas'])
+        return prev_canvas_data, inst_data, next_obj_data, final_canvas_data
+
+    def get_raw_item(self, index):
+        return self.data[index]
+
+    def encode_inst(self, inst_str):
+        inst_data = np.zeros(self.max_seq_length, dtype=np.int64)
+        inst_indices = [self.word_to_ix[w] for w in inst_str.split()]
+        inst_data[:len(inst_indices)] = inst_indices
+        return inst_data
+
+    def encode_obj(self, obj):
+        return np.array((self.color2num[obj['color']],
+                         self.shape2num[obj['shape']],
+                         obj['row'] * 5 + obj['col']), dtype=np.int32)
+
+    def encode_canvas(self, canvas_objs):
+        canvas_data = np.zeros((25, 3), np.float32)
+        for obj in canvas_objs:
+            obj_encode = self.encode_obj(obj)
+            canvas_data[obj_encode[-1]] = obj_encode
+        return canvas_data
+
+    def __len__(self):
+        return len(self.data)
+
+
+def shape2d_collate(data):
+    prev_canvas_data, inst_data, next_obj_data, final_canvas_data = zip(*data)
+    return torch.from_numpy(np.stack(prev_canvas_data)), \
+           torch.from_numpy(np.stack(inst_data)), \
+           torch.from_numpy(np.stack(next_obj_data)), \
+           torch.from_numpy(np.stack(final_canvas_data))
+
+
+def get_shape2d_loader(split, batch_size):
+    assert split in ['train', 'val', 'sample']
+    datafile = {'train': 'train.json', 'val': 'val.json', 'sample': 'sample.json'}[split]
+    dataset = Shape2D(datafile)
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, collate_fn=shape2d_collate)
+    return dataloader
+
+
+def dataset_test():
+    #%%
+    dataset = Shape2D('sample.json')
+    pprint(dataset.vocab)
+    d = dataset.get_raw_item(0)
+    pprint(d)
+    prev_canvas_data, inst_data, next_obj_data, final_canvas_data = dataset[0]
+    pprint(prev_canvas_data)
+    pprint(inst_data)
+    pprint(next_obj_data)
+    pprint(final_canvas_data)
+
+
+def dataloader_test():
+    dataloader = get_shape2d_loader(split='sample', batch_size=3)
+    prev_canvas_data, inst_data, next_obj_data, final_canvas_data = next(iter(dataloader))
+    print('aka')
+
+
+def render_test():
+    config = None
+    config_file = '/net/zf14/xy4cm/Projects/chat-crowd/main/domains/app-2Dshape.json'
+    with open(config_file) as f:
+        config = json.load(f)
+    uri_remote = (config['compose-for-mongodb'][0]['credentials']['uri'] +
+                  '&ssl_cert_reqs=CERT_NONE')
+    cli = MongoClient(uri_remote)
+    mockdb = config["domain-db"]["db-name"]
+    coll_chat = cli[mockdb][config["domain-db"]["coll_chat_data"]]
+    print(coll_chat)
+    task_id = '12557'
+    coll_chat.delete_many({'task_id': task_id})
+    dataset = Shape2D('sample.json')
+    for index in range(len(dataset)):
+        data = dataset.get_raw_item(index)
+        inst = data['current_instruction']
+        coll_chat.insert_one({'msg': inst, 'author': 'human', 'task_id': task_id,
+                          'username': 'aaa', 'worker_id': '123456', "role": 'user', 'turn': str(index), 'mode': '2Dshape',
+                          'timestamp': str(datetime.datetime.now())})
+        objs = data['prev_canvas']
+        objs.append(data['next_object'])
+        coll_chat.insert_one({'msg': render_canvas(objs), 'author': 'human', 'task_id': task_id,
+                          'username': 'bbb', 'worker_id': '123456', "role": 'agent', 'turn': str(index), 'mode': '2Dshape',
+                          'timestamp': str(datetime.datetime.now())})
+
+
+if __name__ == '__main__':
+    dataloader_test()
+
---
+++ painter.py
@@ -0,0 +1,173 @@
+from __future__ import print_function
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+from Shape2D import get_shape2d_loader
+
+# Training settings
+parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+parser.add_argument('--batch-size', type=int, default=256, metavar='N',
+                    help='input batch size for training (default: 64)')
+parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+                    help='input batch size for testing (default: 1000)')
+parser.add_argument('--epochs', type=int, default=10, metavar='N',
+                    help='number of epochs to train (default: 10)')
+parser.add_argument('--lr', type=float, default=1e-4, metavar='LR',
+                    help='learning rate (default: 0.01)')
+parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
+                    help='SGD momentum (default: 0.5)')
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='disables CUDA training')
+parser.add_argument('--seed', type=int, default=1, metavar='S',
+                    help='random seed (default: 1)')
+parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                    help='how many batches to wait before logging training status')
+args = parser.parse_args()
+args.cuda = not args.no_cuda and torch.cuda.is_available()
+
+torch.manual_seed(args.seed)
+torch.cuda.manual_seed(args.seed)
+
+
+class InstEncoder(nn.Module):
+    def __init__(self, vocab_size):
+        super().__init__()
+        self.rnn_size = 64
+        self.input_size = 64
+        # https://github.com/ruotianluo/ImageCaptioning.pytorch/blob/master/models/AttModel.py#L43
+        self.embed = nn.Embedding(vocab_size + 1, self.input_size)
+        # https://github.com/ruotianluo/ImageCaptioning.pytorch/blob/master/models/AttModel.py#L372
+        self.lstm_cell = nn.LSTMCell(self.input_size, self.rnn_size)
+
+    def forward(self, inst):
+        # http://pytorch.org/docs/master/nn.html#torch.nn.LSTMCell
+        batch_size = inst.size(0)
+        h = Variable(torch.zeros(batch_size, self.rnn_size).cuda())
+        c = Variable(torch.zeros(batch_size, self.rnn_size).cuda())
+        for i in range(inst.size(1)):
+            input = self.embed(inst[:, i])
+            h, c = self.lstm_cell(input, (h, c))
+        # only want the last hidden state
+        return h
+
+
+class CanvasEncoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.embed_size = 64
+        self.obj_embed = nn.Linear(3, self.embed_size)
+
+    def forward(self, canvas):
+        # canvas: batch_size x 25 x 3
+        obj_embedding = self.obj_embed(canvas)
+        canvas_embedding = obj_embedding.sum(1)  # batch x 64
+        return canvas_embedding
+
+
+class Shape2DPainterNet(nn.Module):
+    def __init__(self, vocab_size):
+        super().__init__()
+        self.inst_encoder = InstEncoder(vocab_size)
+        self.canvas_encoder = CanvasEncoder()
+        self.hidden_size = 32
+        self.fc1 = nn.Linear(self.inst_encoder.rnn_size + self.canvas_encoder.embed_size, 32)
+        self.fc_color = nn.Linear(32, 3)
+        self.fc_shape = nn.Linear(32, 3)
+        self.fc_loc = nn.Linear(32, 25)
+
+    def forward(self, inst, prev_canvas):
+        inst_embedding = self.inst_encoder(inst)
+        canvas_embedding = self.canvas_encoder(prev_canvas)
+        embedding = torch.cat([inst_embedding, canvas_embedding], 1)
+        x = F.relu(self.fc1(embedding))
+        x = F.dropout(x, training=self.training)
+        color_out = F.log_softmax(self.fc_color(x), dim=1)
+        shape_out = F.log_softmax(self.fc_shape(x), dim=1)
+        loc_out = F.log_softmax(self.fc_loc(x), dim=1)
+        return color_out, shape_out, loc_out
+
+
+class Shape2DObjCriterion(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, model_out, obj_target):
+        color_target = obj_target[:, 0]
+        shape_target = obj_target[:, 1]
+        loc_target = obj_target[:, 2]
+        color_out, shape_out, loc_out = model_out
+        loss = F.nll_loss(color_out, color_target) + \
+               F.nll_loss(shape_out, shape_target) + \
+               F.nll_loss(loc_out, loc_target)
+        return loss
+
+
+def compute_accuracy(model_out, obj_target):
+    obj_target = obj_target.data
+    color_target = obj_target[:, 0]
+    shape_target = obj_target[:, 1]
+    loc_target = obj_target[:, 2]
+    color_out, shape_out, loc_out = model_out[0].data, model_out[1].data, model_out[2].data,
+    batch_size = obj_target.size(0)
+    color_accuracy = torch.eq(torch.max(color_out, dim=1)[1], color_target).sum()/batch_size
+    shape_accuracy = torch.eq(torch.max(shape_out, dim=1)[1], shape_target).sum()/batch_size
+    loc_accuracy = torch.eq(torch.max(loc_out, dim=1)[1], loc_target).sum()/ batch_size
+    return color_accuracy, shape_accuracy, loc_accuracy
+
+train_loader = get_shape2d_loader(split='train', batch_size=args.batch_size)
+test_loader = get_shape2d_loader(split='val', batch_size=args.batch_size)
+assert train_loader.dataset.vocab_size == test_loader.dataset.vocab_size
+assert train_loader.dataset.max_seq_length == test_loader.dataset.max_seq_length
+
+model = Shape2DPainterNet(train_loader.dataset.vocab_size)
+model.cuda()
+loss_fn = Shape2DObjCriterion()
+
+optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+
+
+def train(epoch):
+    model.train()
+    for batch_idx, (prev_canvas, inst, next_obj, final_canvas) in enumerate(train_loader):
+        prev_canvas = Variable(prev_canvas.cuda())
+        inst = Variable(inst.cuda())
+        next_obj = Variable(next_obj.cuda())
+        output = model(inst, prev_canvas)
+        loss = loss_fn(output, next_obj)
+        loss.backward()
+        optimizer.step()
+        color_accuracy, shape_accuracy, loc_accuracy = compute_accuracy(output, next_obj)
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f} {} {} {}'.format(
+                epoch, batch_idx * args.batch_size, len(train_loader.dataset),
+                       100. * batch_idx / len(train_loader), loss.data[0], color_accuracy, shape_accuracy, loc_accuracy))
+
+
+def model_test():
+    model.eval()
+    test_loss = 0
+    correct = 0
+    for prev_canvas, inst, next_obj, final_canvas in test_loader:
+        prev_canvas = Variable(prev_canvas.cuda(), volatile=True)
+        inst = Variable(inst.cuda(), volatile=True)
+        next_obj = Variable(next_obj.cuda(), volatile=True)
+        output = model(inst, prev_canvas)
+        loss = loss_fn(output, next_obj)
+        data, target = Variable(data, volatile=True), Variable(target)
+        output = model(data)
+        test_loss += F.nll_loss(output, target, size_average=False).data[0]  # sum up batch loss
+        pred = output.data.max(1, keepdim=True)[1]  # get the index of the max log-probability
+        correct += pred.eq(target.data.view_as(pred)).long().cpu().sum()
+
+    test_loss /= len(test_loader.dataset)
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+
+for epoch in range(1, args.epochs + 1):
+    train(epoch)
+    # model_test()
