c6b56be6a16754fb827b002fa01d545859507999
songfeng <sfeng@us.ibm.com>
2018-04-12 21:41:41 -0400

    sync add

--- const.py
+++ const.py
@@ -1,16 +1,23 @@
-# mode for generating instructions
-OTHER_ROLE = {'agent': 'user', 'user': 'agent'}  # painter/instructor - agent/user
+OTHER_ROLE = {'agent': 'user', 'user': 'agent'}
+# painter -- agent / instructor -- user
+
+COLORS = ['red', 'green', 'blue']
+SHAPES = ['square', 'circle', 'triangle']
 
 MODE_FULL = 'full'
 MODE_MIN = 'min'
-MODES = [MODE_MIN, MODE_FULL]
+MODES_REF = [MODE_MIN, MODE_FULL]
 
 SINGLE = 'single'
 MULTI = 'multi'
 PATTERN = 'pattern'
+MODES_STYLE = [SINGLE, MULTI, PATTERN]
+
+PATTERN_STYLE = ['row', 'column']
 
 LOC_ABS = 'location_absolute'
 LOC_REL = 'location_relative'
+MODES_LOC = [LOC_ABS, LOC_REL]
 
 
 # state of grid
@@ -29,7 +36,7 @@ ACTIONS = [ADD, DELETE, MOVE]
 
 GRID_SIZE = 5
 
-DICT_ABC_LOC = {
+DICT_NAME2LOC_ABS = {
     'top-left': (0, 0),
     'top-right': (0, GRID_SIZE - 1),
     'bottom-left': (GRID_SIZE - 1, 0),
@@ -37,7 +44,9 @@ DICT_ABC_LOC = {
     'center': (GRID_SIZE // 2, GRID_SIZE // 2)
 }
 
-DICT_DEL_REL_LOC = {
+DICT_LOC_ABS2NAME = {v: k for k, v in DICT_NAME2LOC_ABS.items()}
+
+DICT_LOC_DELTA2NAME = {
     (1, 0): 'top-of',
     (-1, 0): 'bottom-of',
     (0, -1): 'right-of',
@@ -48,13 +57,21 @@ DICT_DEL_REL_LOC = {
     (-1, -1): 'bottom-right-of',
 }
 
-relative_loc_dict = {
-    'top-of': lambda row, col: (row - 1, col),
-    'bottom-of': lambda row, col: (row + 1, col),
-    'right-of': lambda row, col: (row, col + 1),
-    'left-of': lambda row, col: (row, col - 1),
-    'top-left-of': lambda row, col: (row - 1, col - 1),
-    'top-right-of': lambda row, col: (row - 1, col + 1),
-    'bottom-left-of': lambda row, col: (row + 1, col - 1),
-    'bottom-right-of': lambda row, col: (row + 1, col + 1)
-}
+TMPL_ADD = ["add $obj $loc_abs $loc_rel ",
+            "now place $obj $loc_abs $loc_rel "]
+
+TMPL_DEL = ["", ""]
+
+TMPL_MV = ["", ""]
+
+TMPL_LOC_ABS = ["at $loc_abs of the canvas"]
+
+TMPL_LOC_REL = ["at $loc_rel of the $obj_ref"]
+
+TMPL_OBJ_REF = ["the $color $shape $loc_abs $loc_ref"]
+
+DICT_TEMPLATES = {ADD: TMPL_ADD,
+                  DELETE: TMPL_DEL,
+                  MOVE: TMPL_MV,
+                  LOC_ABS: TMPL_LOC_ABS,
+                  LOC_REL: TMPL_LOC_REL}
--- dial_core.py
+++ dial_core.py
@@ -1,9 +1,8 @@
 from const import *
-from domain_2D_shape import *
 from collections import OrderedDict
-from synthetic_layout import Object, Canvas
-
-MODES = [SINGLE, MULTI, PATTERN]
+from synthetic_layout import (Object, Canvas, get_add_activity,
+                              get_delete_activity, get_move_activity,
+                              tmpl2txt_act, tmpl2txt_loc)
 
 
 class Policy(object):
@@ -24,12 +23,36 @@ class Agent(object):
         self.states = OrderedDict
         self.canvas = Canvas()
 
-    def get_act_obj(self, actions=[ADD], mode=None):
-        if not mode:
-            mode = random.choice(MODES)
+    def get_act_obj(self, actions=[ADD], mode_loc=None, mode_style=None):
+        if not mode_style:
+            mode_style = random.choice(MODES_STYLE)
+        if not mode_loc:
+            mode_loc = random.choice(MODES_LOC)
+        lst_activity = []
         for action in actions:
+            if action == ADD:
+                activity = get_add_activity(self.canvas, mode_loc, mode_style)
+            if action == DELETE:
+                activity = get_delete_activity(self.canvas, mode_loc, mode_style)
+            if action == MOVE:
+                activity = get_move_activity(self.canvas, mode_loc, mode_style)
+            if activity:
+                lst_activity.append((action,) + activity)
+        return lst_activity
 
-
+    def generate_act_message_by_tmpl(self, lst_activity, mode_ref=None):
+        if not mode_ref:
+            mode_ref = random.choice(MODES_REF)
+        lst = []
+        for act, obj, loc_abs, loc_rel, obj_ref in lst_activity:
+            t_loc_abs = t_loc_rel = ''
+            if loc_abs:
+                t_loc_abs = tmpl2txt_loc(loc_abs, loc_rel, obj_ref, LOC_ABS)
+            if loc_rel and obj_ref:
+                t_loc_rel = tmpl2txt_loc(loc_abs, loc_rel, obj_ref, LOC_REF)
+            color, shape, loc_abs, loc_ref, obj_ref = canvas.?
+            t_obj = tmpl2txt_obj(color, shape, loc_abs, loc_ref, obj_ref)
+            message = tmpl2txt_act(ADD, t_obj, t_loc_abs, t_loc_rel)
 
     def get_next_message(self, actions_pre, dial_act_pre, role_pre):
         actions = []
