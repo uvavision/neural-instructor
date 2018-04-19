5e24d504ee19c8684965cd4068434eb7699977dc
Xuwang Yin <xy4cm@virginia.edu>
2018-04-19 23:03:52 +0000

    cleanup

--- canvas_render.py
+++ canvas_render.py
@@ -3,7 +3,7 @@ from flask import Flask, request, render_template
 from pprint import pprint
 app = Flask(__name__)
 
-tasks = None
+instructor_predictions = None
 
 # def make_public_task(task):
 #     new_task = {}
@@ -14,7 +14,7 @@ tasks = None
 #             new_task[field] = task[field]
 #     return new_task
 
-tasks = [{
+instructor_predictions = [{
              'prev_canvas': '#CANVAS-[{"left":10,"top":10,"width":80,"height":80,"label":"blue","shape":"rectangle"},{"left":410,"top":10,"width":80,"height":80,"label":"green","shape":"rectangle"},{"left":10,"top":410,"width":80,"height":80,"label":"green","shape":"circle"},{"left":10,"top":110,"width":80,"height":80,"label":"red","shape":"triangle"},{"left":210,"top":210,"width":80,"height":80,"label":"blue","shape":"rectangle"}]',
              'current_instruction': 'add a blue square to bottom-of the blue square on center of the canvas',
              'next_object': {'row': 3, 'col': 2, 'color': 'blue', 'shape': 'square'},
@@ -28,18 +28,31 @@ tasks = [{
              'final_canvas': '#CANVAS-[{"left":10,"top":410,"width":80,"height":80,"label":"red","shape":"triangle"},{"left":110,"top":410,"width":80,"height":80,"label":"blue","shape":"rectangle"},{"left":10,"top":10,"width":80,"height":80,"label":"green","shape":"circle"},{"left":210,"top":310,"width":80,"height":80,"label":"blue","shape":"rectangle"},{"left":410,"top":410,"width":80,"height":80,"label":"green","shape":"rectangle"},{"left":210,"top":210,"width":80,"height":80,"label":"green","shape":"rectangle"},{"left":310,"top":310,"width":80,"height":80,"label":"red","shape":"triangle"},{"left":410,"top":10,"width":80,"height":80,"label":"red","shape":"triangle"},{"left":10,"top":310,"width":80,"height":80,"label":"green","shape":"rectangle"},{"left":110,"top":310,"width":80,"height":80,"label":"green","shape":"rectangle"}]',
              'predicted_instruction': 'add a red triangle to top-of the red triangle'}]
 
+dialog_sample = []
+
 
 @app.route("/")
-def hello():
-    return render_template('history.html', data=tasks)
+def instructor():
+    return render_template('instructor_predict.html', data=instructor_predictions)
+
+
+@app.route("/dialog")
+def dialog():
+    return render_template('dialog.html', data=dialog_sample)
 
 
 @app.route('/new_task', methods=['POST'])
 def create_task():
-    global tasks
-    tasks = request.json
+    global instructor_predictions
+    instructor_predictions = request.json
     return "OK"
 
+
+@app.route('/new_dialog', methods=['POST'])
+def new_dialog():
+    global dialog_sample
+    dialog_sample = request.json
+    return "OK"
 
 
 if __name__ == '__main__':
